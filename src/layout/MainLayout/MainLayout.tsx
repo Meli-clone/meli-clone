8f010ccfc60fbefd9725ebde70ead8033546a73b
cristiannd <cristian.donalicio@gmail.com>
2022-09-20 12:21:51 -0300

    Add the Regret component

---
+++ src/layout/MainLayout/MainLayout.tsx
@@ -0,0 +1,11 @@
+import Home from '@/views/Home';
+
+const MainLayout = () => {
+  return (
+    <>
+      <Home />
+    </>
+  );
+};
+
+export default MainLayout;
---
+++ src/views/Home/Home.tsx
@@ -0,0 +1,12 @@
+import Regret from './components/Regret/Regret';
+import './Home.scss';
+
+const Home = () => {
+  return (
+    <div>
+      <Regret />
+    </div>
+  );
+};
+
+export default Home;
