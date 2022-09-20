422b13432f505dc33757cb75d17442d736326fd7
diaslucia <luciadias.dev@gmail.com>
2022-09-20 19:44:14 -0300

    Add homePayment html, scss and test. Add images in assets and queries in theme.scss.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 const App = () => {
   return (
-    <>
+    <div style={{display: "flex", justifyContent: "center"}}>
       <header></header>
       <main>Mercado Libre</main>
       <footer></footer>
-    </>
+    </div>
   );
 };
 
