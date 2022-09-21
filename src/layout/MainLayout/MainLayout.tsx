0af77e947fe68846583d02af7a889b24896f3d4a
diaslucia <luciadias.dev@gmail.com>
2022-09-21 14:23:43 -0300

    Add descubre components, styles, test and index

--- src/layout/MainLayout/MainLayout.tsx
+++ src/layout/MainLayout/MainLayout.tsx
@@ -1,10 +1,12 @@
 import HomePayment from '@/components/HomePayment';
+import Descubre from '@/components/Descubre/Descubre';
 import Footer from '../Footer';
 
 const MainLayout = () => {
   return (
     <>
       <HomePayment/>
+      <Descubre/>
       <Footer />
     </>
   );
