6017dfe8ea7120195e5663c0d4fe607c94f8dada
diaslucia <luciadias.dev@gmail.com>
2022-09-22 10:41:58 -0300

    Update develop branch

--- src/layout/MainLayout/MainLayout.tsx
+++ src/layout/MainLayout/MainLayout.tsx
@@ -1,10 +1,12 @@
 import HomePayment from '@/components/HomePayment';
+import ProductList from '@/components/ProductList';
 import Footer from '../Footer';
 
 const MainLayout = () => {
   return (
     <>
       <HomePayment/>
+      <ProductList/>
       <Footer />
     </>
   );
