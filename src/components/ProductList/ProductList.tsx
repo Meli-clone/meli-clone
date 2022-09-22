4d8e21fbb0e998017cc05739a24c05a2262a85dd
diaslucia <luciadias.dev@gmail.com>
2022-09-22 13:35:56 -0300

    Style product card

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -4,63 +4,63 @@ import ProductListCard from './components/ProductListCard';
 const PRODUCTS_LIST = [
   {
     item_id: 1,
-    title: 'Samsung Galaxy A22 5G 128 GB gray 4 GB RAM',
+    title: 'Samsung Galaxy A22 5G 128 GB gray 4 GB RAM aaaaaaaaaaaaaaaaaaaaa aaaa aa asdas das d',
     image: 'https://http2.mlstatic.com/D_NQ_NP_863356-MLA48697556226_122021-O.webp',
-    price: 59000
+    price: '59.000'
   },
   {
     item_id: 2,
     title: 'Samsung Galaxy A22 5G 128 GB gray 4 GB RAM',
     image: 'https://http2.mlstatic.com/D_NQ_NP_863356-MLA48697556226_122021-O.webp',
-    price: 59000
+    price: '59.000'
   },
   {
     item_id: 3,
     title: 'Samsung Galaxy A22 5G 128 GB gray 4 GB RAM',
     image: 'https://http2.mlstatic.com/D_NQ_NP_863356-MLA48697556226_122021-O.webp',
-    price: 59000
+    price: '59.000'
   },
   {
     item_id: 4,
     title: 'Samsung Galaxy A22 5G 128 GB gray 4 GB RAM',
     image: 'https://http2.mlstatic.com/D_NQ_NP_863356-MLA48697556226_122021-O.webp',
-    price: 59000
+    price: '59.000'
   },
   {
     item_id: 5,
     title: 'Samsung Galaxy A22 5G 128 GB gray 4 GB RAM',
     image: 'https://http2.mlstatic.com/D_NQ_NP_863356-MLA48697556226_122021-O.webp',
-    price: 59000
+    price: '59.000'
   },
   {
     item_id: 6,
     title: 'Samsung Galaxy A22 5G 128 GB gray 4 GB RAM',
     image: 'https://http2.mlstatic.com/D_NQ_NP_863356-MLA48697556226_122021-O.webp',
-    price: 59000
+    price: '59.000'
   },
   {
     item_id: 7,
     title: 'Samsung Galaxy A22 5G 128 GB gray 4 GB RAM',
     image: 'https://http2.mlstatic.com/D_NQ_NP_863356-MLA48697556226_122021-O.webp',
-    price: 59000
+    price: '59.000'
   },
   {
     item_id: 8,
     title: 'Samsung Galaxy A22 5G 128 GB gray 4 GB RAM',
     image: 'https://http2.mlstatic.com/D_NQ_NP_863356-MLA48697556226_122021-O.webp',
-    price: 59000
+    price: '59.000'
   },
   {
     item_id: 9,
     title: 'Samsung Galaxy A22 5G 128 GB gray 4 GB RAM',
     image: 'https://http2.mlstatic.com/D_NQ_NP_863356-MLA48697556226_122021-O.webp',
-    price: 59000
+    price: '59.000'
   },
   {
     item_id: 10,
     title: 'Samsung Galaxy A22 5G 128 GB gray 4 GB RAM',
     image: 'https://http2.mlstatic.com/D_NQ_NP_863356-MLA48697556226_122021-O.webp',
-    price: 59000
+    price: '59.000'
   },
 ];
 
--- src/components/ProductList/components/ProductListCard.tsx
+++ src/components/ProductList/components/ProductListCard.tsx
@@ -1,10 +1,25 @@
 import './ProductListCard.scss';
+import { useState } from "react";
 
 const ProductListCard = ({producto}) => {
+  const [productHover, setProductHover] = useState(false);
+
+  const handleMouseOver = () => {
+    setProductHover(true);
+  }
+
+  const handleMouseOut = () => {
+    setProductHover(false);
+  }
+
   return(
-        <div className="productListCard">
+        <div className="productListCard" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
             <div className="productListCard_imageContainer">
                 <img className="productListCard_image" src={producto.image} alt={producto.title}/>
+            </div>
+            <div className="productListCard_infoContainer">
+              <h4 className="productListCard_price">$ {producto.price}</h4>
+              <p className="productListCard_title" style={{opacity: productHover ? '1' : '0'}}>{producto.title}</p>
             </div>
         </div>
   ) ;
