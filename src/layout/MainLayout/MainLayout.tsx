6d85a498aa3b185b9052d93d5c7a885107e24f6f
Lucy <88150989+diaslucia@users.noreply.github.com>
2022-09-22 12:18:34 -0300

    Descubre (#5)
    * Add descubre components, styles, test and index.
    * Fix hover and transition styles in Descubre and HomePayment. Add background color in body. Change footer background color to white.

--- src/layout/MainLayout/MainLayout.tsx
+++ src/layout/MainLayout/MainLayout.tsx
@@ -1,12 +1,14 @@
 import PromotionsBanner from '@/components/PromotionsBanner/PromotionsBanner';
 import HomePayment from '@/components/HomePayment';
+import Descubre from '@/components/Descubre/Descubre';
 import Footer from '../Footer';
 
 const MainLayout = () => {
   return (
     <>
+      <HomePayment/>
+      <Descubre/>
       <PromotionsBanner />
-      <HomePayment />
       <Footer />
     </>
   );
