5b9d81d7685f3c40d218e8b6c25e097cf3f4606d
Leonardo Diaz <87768747+leodiazv@users.noreply.github.com>
2022-09-30 09:39:56 -0500

    Login (#16)
    * Add login component with mobile first styles
    
    * Add tablet styles
    
    * Add laptop styles
    
    * Delete caret for dependencies
    
    Co-authored-by: Leonardo Diaz <leodiazv@gmail.como>

--- src/views/Login/Login.tsx
+++ src/views/Login/Login.tsx
@@ -1,5 +1,129 @@
+import { Formik, Form, FormikHelpers } from 'formik';
+import { Link, useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
+
+//COMPONENTS AND FUNCTIONS
+import CustomInput from './components/CustomInput';
+import { loginSchema } from './schemas/loginSchemas';
+
+//STYLES AND IMAGES
+import meliLogo2 from '@/assets/images/meli-logo2.svg';
+import './Login.scss';
+
+//ICONS
+import { GiSmartphone } from 'react-icons/gi';
+import { RiUser3Line } from 'react-icons/ri';
+import { MdOutlineKeyboardArrowRight } from 'react-icons/md';
+import { TbHelp } from 'react-icons/tb';
+
+interface FormValues {
+  username: string;
+  password: string;
+}
+
 const Login = () => {
-  return <div>Login</div>;
+  const initialValues: FormValues = {
+    username: '',
+    password: '',
+  };
+
+  const navigate = useNavigate();
+
+  const onSubmit = (
+    values: FormValues,
+    formikBag: FormikHelpers<FormValues>,
+  ) => {
+    console.log({ values, formikBag });
+    if (values.username !== 'jhon' || values.password !== '12345') {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Credenciales invalidas 🤔',
+        footer: '<a href="">Has olvidado la contraseña?</a>',
+      });
+    } else {
+      localStorage.setItem('username', values.username);
+      navigate('/');
+    }
+    formikBag.setSubmitting(false);
+  };
+
+  return (
+    <div className='login'>
+      <header className='login_header'>
+        <div className='login_header__container'>
+          <Link to='/' className='login_header__container__logo'>
+            <img src={meliLogo2}></img>
+          </Link>
+          <Link to='#' className='login_header__container__help'>
+            <TbHelp className='help_icon' />
+          </Link>
+        </div>
+      </header>
+      <div className='main_container'>
+        <div className='main_container__main_card'>
+          <h1>¡Hola! Ingresa tu teléfono, e-mail o usuario</h1>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={loginSchema}
+            onSubmit={onSubmit}
+            className='main_card__login_form'
+          >
+            {({ isSubmitting }) => (
+              <Form>
+                <CustomInput
+                  label='Teléfono, e-mail o usuario'
+                  name='username'
+                  type='text'
+                  placeholder=''
+                />
+
+                <CustomInput
+                  label='Contraseña'
+                  name='password'
+                  type='password'
+                  placeholder=''
+                />
+
+                <button type='submit' disabled={isSubmitting}>
+                  Continuar
+                </button>
+              </Form>
+            )}
+          </Formik>
+          <div className='main_container__main_card__registration_link'>
+            <Link to='#'>Crear cuenta</Link>
+          </div>
+          <div className='main_container__main_card__help_link'>
+            <Link to='#'>Necesito ayuda para ingresar</Link>
+          </div>
+        </div>
+        <div className='main_container__security_issues'>
+          <p>Reportar un problema</p>
+          <ul>
+            <li>
+              <div>
+                <GiSmartphone />
+                Robo o pérdida de teléfono
+              </div>
+              <MdOutlineKeyboardArrowRight />
+            </li>
+            <li>
+              <div>
+                <RiUser3Line />
+                Robo de cuenta
+              </div>
+              <MdOutlineKeyboardArrowRight />
+            </li>
+          </ul>
+        </div>
+      </div>
+      <footer>
+        <Link to='#'>Cómo cuidamos tu privacidad</Link>
+        <p>Copyright © 1999-2022 MercadoLibre Colombia LTDA.</p>
+      </footer>
+    </div>
+  );
 };
 
 export default Login;
