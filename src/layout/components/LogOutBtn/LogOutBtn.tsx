c32d0c8e1a14ad4b32e30136e3278305121e3fb4
EliasPaladino <69222651+EliasPaladino@users.noreply.github.com>
2022-10-19 10:47:43 -0300

    Add slice deleteAll and deleteAllSummary (#34)

--- src/layout/components/LogOutBtn/LogOutBtn.tsx
+++ src/layout/components/LogOutBtn/LogOutBtn.tsx
@@ -1,4 +1,6 @@
+import { deleteAll } from '@/store/cart/cartSlice';
 import { useAppDispatch } from '@/store/hooks';
+import { deleteAllSummary } from '@/store/summary/summarySlice';
 
 //COMPONENTS AND FUNCTIONS
 import { logout } from '@/store/user/user.slice';
@@ -9,6 +11,8 @@ const LogOutBtn = () => {
   const logoutHandler = () => {
     localStorage.clear();
     dispatch(logout());
+    dispatch(deleteAll());
+    dispatch(deleteAllSummary());
   };
   return <button onClick={() => logoutHandler()}>Salir</button>;
 };
