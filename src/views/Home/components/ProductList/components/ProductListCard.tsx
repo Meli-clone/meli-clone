1ba2f81e30a2fa7d0dfe5137020061d4130a730f
diaslucia <luciadias.dev@gmail.com>
2022-10-20 20:15:54 -0300

    Style home gap mobile and change logo

--- src/views/Home/components/ProductList/components/ProductListCard.tsx
+++ src/views/Home/components/ProductList/components/ProductListCard.tsx
@@ -1,40 +1,61 @@
 import './ProductListCard.scss';
-import { useState } from "react";
+import { useState } from 'react';
 import { BsHeart } from 'react-icons/bs';
+import formatNumberWithDot from '@/utils/helpers/formatNumberWithDot';
 
 interface Products {
-  item_id: number
-  title: string
-  image: string
-  price: string
+  item_id: number;
+  title: string;
+  image: string;
+  price: string;
 }
 
 interface Props {
-  producto: Products
+  producto: Products;
 }
 
-const ProductListCard = ({producto}: Props) => {
+const ProductListCard = ({ producto }: Props) => {
   const [productHover, setProductHover] = useState(false);
 
   const handleMouseOver = () => {
     setProductHover(true);
-  }
+  };
 
   const handleMouseOut = () => {
     setProductHover(false);
-  }
+  };
 
-  return(
-  <div className="productListCard" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
-      <button className="productListCard_button" style={{opacity: productHover ? '1' : '0'}}><BsHeart className="productListCard_icon"/></button>
-      <div className="productListCard_imageContainer">
-          <img className="productListCard_image" src={producto.image} alt={producto.title}/>
+  return (
+    <div
+      className='productListCard'
+      onMouseOver={handleMouseOver}
+      onMouseOut={handleMouseOut}
+    >
+      <button
+        className='productListCard_button'
+        style={{ opacity: productHover ? '1' : '0' }}
+      >
+        <BsHeart className='productListCard_icon' />
+      </button>
+      <div className='productListCard_imageContainer'>
+        <img
+          className='productListCard_image'
+          src={producto.image}
+          alt={producto.title}
+        />
       </div>
-      <div className="productListCard_infoContainer">
-        <h4 className="productListCard_price">$ {producto.price}</h4>
-        <p className="productListCard_title" style={{opacity: productHover ? '1' : '0'}}>{producto.title}</p>
+      <div className='productListCard_infoContainer'>
+        <h4 className='productListCard_price'>
+          $ {formatNumberWithDot(Number(producto.price))}
+        </h4>
+        <p
+          className='productListCard_title'
+          style={{ opacity: productHover ? '1' : '0' }}
+        >
+          {producto.title}
+        </p>
       </div>
-  </div>
+    </div>
   );
 };
 
